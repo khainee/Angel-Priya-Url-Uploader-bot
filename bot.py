afbb054576fd004b38de31e0b8f214ee215ee8a1
khainee <118953358+khainee@users.noreply.github.com>
2023-02-08 11:54:23 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,5 +34,5 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(1484670284)
+    Config.AUTH_USERS.add(-1001759801488)
     app.run()
